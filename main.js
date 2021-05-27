45391191ffc58d11140aff3bae88b46871db9a27
techmahendran <mahendran361999@gmail.com>
2021-05-27 12:09:55 +0530

    change css, js code

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-let fname = document.querySelector('.name')
-let lname = document.querySelector('.lname')
-let input = document.querySelector('.einput')
-let item = document.querySelector('#item')
-let add = document.querySelector('#add')
+let fname = document.querySelector('.name');
+let lname = document.querySelector('.lname');
+let input = document.querySelector('.einput');
+let item = document.querySelector('#item');
+let add = document.querySelector('#add');
 
+let btn = document.querySelector('#btn');
 
-let btn = document.querySelector('#btn')
-
-btn.addEventListener('click', handeClick)
+btn.addEventListener('click', handeClick);
 
 function handeClick(e) {
     e.preventDefault();
-    // fname.classList.add('box')
     if (fname.value === '' || lname.value === '') {
-        item.classList.add('error')
-        item.innerHTML = 'Please enter the fileds'
+        item.classList.add('error');
+        item.innerHTML = 'Please enter the fileds';
 
-        setTimeout(() => item.remove(),3000)
+        setTimeout(() => item.remove(),3000);
     
     } else {
-        let para = document.createElement('p')
-        para.appendChild(document.createTextNode(`${fname.value} : ${lname.value} : ${input.value}`))
-        add.appendChild(para)
+        let para = document.createElement('p');
+        para.appendChild(document.createTextNode(`${fname.value} : ${lname.value} : ${input.value}`));
+        add.appendChild(para);
     }
 }
